2b4dd5eae122e75d300f71fe7ae4bfc3848a9f06
prakash587 <80191742+prakash587@users.noreply.github.com>
2025-05-09 23:55:20 +0545

    Added waitress and prepared app for Render deployment

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 from flask import Flask, render_template, request
-
 import pandas as pd
 import pickle
 import numpy as np
 import waitress
 
-
 app = Flask(__name__)
 data = pd.read_csv('Cleanded_data.csv')
 pipe = pickle.load(open('RidgeModel.pkl', 'rb'))
 
-
 @app.route('/')
 def index():
-    locations= sorted(data['location'].unique())
-
+    locations = sorted(data['location'].unique())
     return render_template('index.html', locations=locations)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     location = request.form.get('location')
     bhk = request.form.get('bhk')
-    bath= request.form.get('bath')
+    bath = request.form.get('bath')
     sqft = request.form.get('total_sqft')
 
     print(location, bhk, bath, sqft)
-    input= pd.DataFrame([[location, bhk, bath, sqft]],columns=['location','bhk','bath','total_sqft'])
-    prediction= pipe.predict(input)[0] * 1e5
-    return str(np.round(prediction,2))
-
-
-
+    input = pd.DataFrame([[location, bhk, bath, sqft]], columns=['location', 'bhk', 'bath', 'total_sqft'])
+    prediction = pipe.predict(input)[0] * 1e5
+    return str(np.round(prediction, 2))
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    # Use waitress for production
+    waitress.serve(app, host='0.0.0.0', port=5001)
